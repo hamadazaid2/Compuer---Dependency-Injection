094147ddee3d19727bed0e103dbae62e9c37fd3f
hamadazaid2 <hamadazaid62@gmail.com>
2023-04-08 11:57:16 +0300

    Deploy

---
+++ src/power/power.service.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PowerService {
+  supplyPower(watts: number) {
+    console.log(`Supplying ${watts} worth of power.`);
+  }
+}
